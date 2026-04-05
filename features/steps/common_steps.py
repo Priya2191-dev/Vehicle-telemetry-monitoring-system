5fd3de503ad1627ae6a46e30cc7292650c718fe5
Priya2191-dev <lalepriyanka@gmail.com>
2026-04-05 20:41:31 +0530

    Update common_steps.py

--- features/steps/common_steps.py
+++ features/steps/common_steps.py
@@ -3,3 +3,9 @@ from behave import then
 @then('an error should occur')
 def step_error(context):
     assert context.error is not None, "Expected error but none occurred"
+    
+# fast_api
+@then('an error should occur')
+def step_error(context):
+    assert context.response is not None
+    assert context.response.status_code in [400, 422]
