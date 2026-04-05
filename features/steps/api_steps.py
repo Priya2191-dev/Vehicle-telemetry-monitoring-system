016b9764e5d7b250cb0a6e079768ab2263d9eb5e
Priya2191-dev <lalepriyanka@gmail.com>
2026-04-05 20:40:29 +0530

    Update api_steps.py

--- features/steps/api_steps.py
+++ features/steps/api_steps.py
@@ -6,27 +6,23 @@ client = TestClient(app)
 
 
 @when('I call speed API')
-def step_speed(context):
+def when_speed(context):
     context.response = client.get("/speed")
 
 @when('I call health API')
-def step_health(context):
+def when_health(context):
     context.response = client.get("/health")
 
 @when('I call speed API with invalid data')
-def step_invalid(context):
+def when_invalid(context):
     context.response = client.get("/speed?data=fast")
 
 @then('response should contain average')
-def step_validate_avg(context):
+def then_validate_avg(context):
     assert context.response.status_code == 200
     assert "average" in context.response.json()
 
 @then('status should be running')
-def step_validate_health(context):
+def then_validate_health(context):
     assert context.response.status_code == 200
     assert context.response.json()["status"] == "running"
-
-@then('an error should occur')
-def step_error(context):
-    assert context.response.status_code in [400, 422]
